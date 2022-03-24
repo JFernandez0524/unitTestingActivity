0daa0a43fcd7c288b34006024cf8169151434e02
JFernandez0524 <njphonefix@gmail.com>
2022-03-24 14:38:44 -0400

    2 tests working

--- greet.js
+++ greet.js
@@ -1,7 +1,24 @@
 function greet(name) {
+  if (name === null) {
+    return `Hello there!`;
+  }
+  // if (Array.isArray(name) && name.length <= 2) {
+  //   return `Hello,${name[0]},${name[1]}`;
+  // }
+  if (Array.isArray(name)) {
+    let names = name.join();
+    return `Hello,${names}`;
+  }
+
+  if (name === name.toUpperCase()) {
+    return `HELLO ${name}!`;
+  }
+
   return 'Hello, ' + name;
 }
 
-greet('Elizabeth');
+console.log(greet('Elizabeth'));
+console.log(greet(null));
 
+// export { greet };
 module.exports = greet;
--- greet.test.js
+++ greet.test.js
@@ -1,8 +1,14 @@
+const { name } = require('ejs');
 const greet = require('./greet');
 
 describe('test greet()', () => {
-  it('Should be called', () => {
+  test('name = Elizabeth', () => {
     // const result = 'Hello, Elizabeth';
-    expect(greet('Elizabeth')).toBe('Hello, Elizabeth');
+    expect(greet('Elizabeth')).toMatch('Elizabeth');
+  });
+
+  test('name is null', () => {
+    // const name = null;
+    expect(greet(null)).toMatch('Hello there!');
   });
 });
